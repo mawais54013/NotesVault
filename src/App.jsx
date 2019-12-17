30232e09ec138d173162920de2efb12db89570a0
MAwais <mawais54013@outlook.com>
2019-12-16 20:45:42 -0800

    added note additonal

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Note from "./components/Note";
 import CreateArea from "./components/CreateArea";
 
 function App() {
+
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes(prevNote => {
+      console.log(prevNote)
+      return [...prevNote, newNote];
+    })
+  }
+
   return (
     <div>
       <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem) => {
+        return <Note title={noteItem.title} content={noteItem.content} />
+      })}
       <Footer />
     </div>
   );
